8f1691c3d361f4a9597603e5d08e2da83182dbcb
Luis Diaz <luisdiazugena@gmail.com>
2015-09-30 18:26:42 +0200

    added lcd i2c support

--- sf-bqlock/sf-bqlock.ino
+++ sf-bqlock/sf-bqlock.ino
@@ -1,49 +1,69 @@
 /*
-  Título: RTC_Arduino
-  Descripción: Uso básico del reloj RTC DS1307
-  Autor: FjRamirez
-  Fecha: 9/09/2014
-  URL: www.tuelectronica.es
-  email: email@tuelectronica.es
+  bqlock
+  Julian Caro
+  Luis Díaz
+
+  diwo.bq.com
 */
 
+//libraries
 #include <Wire.h>
 #include "RTClib.h"
+#include "bqLiquidCrystal.h"
+
+/*#define I2C_ADDR_LCD    0x20
+#define I2C_ADDR_RTC    0x68*/
 
 //Clock variables
 RTC_DS1307 RTC;
-
-
-//Encoder Varibles
-
+LiquidCrystal lcd(0);
 
 void setup () {
+  Serial.begin(19200); // Establece la velocidad de datos del puerto serie
+
   Wire.begin();
   RTC.begin();
+
+  lcd.begin(16,2);
+  lcd.display();
+  lcd.setBacklight(HIGH);
+  lcd.blink();
+  lcd.print("Hola Mundo I2C!");
   //RTC.adjust(DateTime(__DATE__, __TIME__)); // Establece la fecha y hora (Comentar una vez establecida la hora)
-  Serial.begin(19200); // Establece la velocidad de datos del puerto serie
+  delay(1000);
+  lcd.clear();
+  lcd.home();
 }
 
 void loop() {
-
-
-
+  getTime();
 }
 
 void getTime(){
+  //lcd.clear();
+  lcd.home();
   DateTime now = RTC.now(); // Obtiene la fecha y hora del RTC
 
-  Serial.print(now.year(), DEC); // Year
-  Serial.print('/');
-  Serial.print(now.month(), DEC); // Month
-  Serial.print('/');
-  Serial.print(now.day(), DEC); // Day
-  Serial.print(' ');
-  Serial.print(now.hour(), DEC); // Hour
-  Serial.print(':');
-  Serial.print(now.minute(), DEC); // Minute
-  Serial.print(':');
-  Serial.print(now.second(), DEC); // Second
-  Serial.println();
+  lcd.print(now.day(), DEC); // Day
+  lcd.print('/');
+  lcd.print(now.month(), DEC); // Month
+  lcd.print('/');
+  lcd.print(now.year(), DEC); // Year
+  lcd.print(' ');
+  lcd.setCursor(0,1);
+  lcd.print(now.hour(), DEC); // Hour
+  lcd.print(':');
+  lcd.print(now.minute(), DEC); // Minute
+  lcd.print(':');
+  checkDigits(now.second());
+  //lcd.print(now.second(), DEC); // Second
 
+}
+void checkDigits(int value){
+  if(value<10){
+    lcd.print(0);
+    lcd.print(value);
+  }else{
+    lcd.print(value);
+  }
 }
